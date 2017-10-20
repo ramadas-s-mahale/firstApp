dad55520ea073b4ff4286a75385bd06ba29f7761
Ramadas <naruto.mahale@gmail.com>
2017-10-20 15:39:55 +0530

    Trying again

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,4 @@
-function scanBarcode{
+function scanBarcode(){
 	
 	cordova.plugins.barcodeScanner.scan(
       function (result) {
@@ -15,7 +15,7 @@ function scanBarcode{
           showFlipCameraButton : true, // iOS and Android
           showTorchButton : true, // iOS and Android
           torchOn: true, // Android, launch with the torch switched on (if available)
-          saveHistory: true // Android, save scan history (default false)
+          saveHistory: true, // Android, save scan history (default false)
           prompt : "Place a barcode inside the scan area", // Android
           resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
           formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
